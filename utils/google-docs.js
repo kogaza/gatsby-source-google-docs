505e63be1e16625a9d4cf5be280bfc1415687692
Krzysztof Ogaza <krzysztofogaza@gmail.com>
2023-09-22 09:28:34 +0200

    clg

--- utils/google-docs.js
+++ utils/google-docs.js
@@ -17,7 +17,7 @@ async function fetchDocument(id) {
     documentId: id,
   })
   console.log('res title: ', res.data.title);
-  console.log('res body: ', res.data.body);
+  console.log('res body: ', res.data.body, res.data.body.content.foreach(el => console.log(el.paragraph)));
   console.log('res styles: ', res.data.documentStyle);
 
   if (!res.data) {
