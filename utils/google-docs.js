378deeae5428b10b303851385aed244e1a4be3e3
Krzysztof Ogaza <krzysztofogaza@gmail.com>
2023-09-22 12:15:54 +0200

    fix

--- utils/google-docs.js
+++ utils/google-docs.js
@@ -10,7 +10,7 @@ const exportData = (data) => {
   const jsonString = `data:text/json;chatset=utf-8,${encodeURIComponent(
     JSON.stringify(data)
   )}`;
-  const link = windows.document.createElement("a");
+  const link = window.document.createElement("a");
   link.href = jsonString;
   link.download = "data.json";
 
